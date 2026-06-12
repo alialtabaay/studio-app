513459e4d3f68ea082681d10c87da5d05898cb6a
alialtabaay <alialtabaay0@gmail.com>
2026-06-12 14:51:46 +0300

    Update app_v3.py

--- app_v3.py
+++ app_v3.py
@@ -276,7 +276,7 @@ else:
         with tab_add_manual:
             st.markdown("### ➕ إضافة معدة جديدة")
             
-            with st.form("add_inventory_form", clear_on_submit=True):
+            with st.form("add_inventory_form"):
                 col1, col2 = st.columns(2)
                 
                 with col1:
@@ -512,7 +512,7 @@ else:
         with tab_add_manual:
             st.markdown("### ➕ إضافة معدة جديدة")
             
-            with st.form("add_inventory_form", clear_on_submit=True):
+            with st.form("add_inventory_form"):
                 col1, col2 = st.columns(2)
                 
                 with col1:
